3089a2ceeb6941b6fd4c1fc9864999b78f4a9944
alvarmig <43011534+alvarmig@users.noreply.github.com>
2019-10-07 20:09:41 -0500

    Updates

---
+++ precache-manifest.97e24ecb880b37e26ce78dc34287e03b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f42edb027856e56963721b7d196d15d",
+    "url": "/React-Fundamentals/index.html"
+  },
+  {
+    "revision": "dcc73ab90213a8494f60",
+    "url": "/React-Fundamentals/static/css/main.21c8462e.chunk.css"
+  },
+  {
+    "revision": "c996231df6b7b29ede21",
+    "url": "/React-Fundamentals/static/js/2.8bb4d0cc.chunk.js"
+  },
+  {
+    "revision": "dcc73ab90213a8494f60",
+    "url": "/React-Fundamentals/static/js/main.76f691be.chunk.js"
+  },
+  {
+    "revision": "f404e6e98525ccd21dad",
+    "url": "/React-Fundamentals/static/js/runtime-main.ac89de19.js"
+  }
+]);
